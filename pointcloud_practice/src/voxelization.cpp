50e8dd56154bbc446377e3a49cbd3ee32400911a
Hyun-je <bigae2@gmail.com>
2021-05-29 05:16:16 +0000

    create package

---
+++ pointcloud_practice/src/voxelization.cpp
@@ -0,0 +1,50 @@
+#include "ros/ros.h"
+#include "sensor_msgs/PointCloud2.h"
+
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
+
+#include "pcl/filters/voxel_grid.h"
+
+
+ros::Publisher pub;
+
+
+
+void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& pointCloud2) {
+
+	pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
+
+	// Convert ROS PointCloud2 to PCL PointCloud
+	pcl::fromROSMsg(*pointCloud2, *pcl_cloud);
+
+	// Voxelize PCL PointCloud
+	pcl::VoxelGrid<pcl::PointXYZI> sor;
+	sor.setInputCloud (pcl_cloud);
+	sor.setLeafSize (0.5f, 0.5f, 0.5f);
+	sor.filter (*pcl_cloud_filtered);
+
+	// Make Filtered PointCloud2 Message
+	sensor_msgs::PointCloud2 pointCloud2_out;
+	pointCloud2_out.header.frame_id = "velodyne";
+	pcl::toROSMsg(*pcl_cloud_filtered, pointCloud2_out);
+
+	// Publish Message
+	pub.publish(pointCloud2_out);
+
+}
+
+
+int main(int argc, char** argv) {
+
+	ros::init(argc, argv, "voxelization");
+	ros::NodeHandle nh;
+
+	ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/velodyne_points", 1000, pointCloudCallback);
+	pub = nh.advertise<sensor_msgs::PointCloud2> ("/velodyne_points_voxelized", 1000);
+
+	ros::spin();
+
+}
